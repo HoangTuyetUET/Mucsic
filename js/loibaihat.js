334b14d6c94b8e6ce48022beca8596c61ac9d27c
HoangTuyetUET <tuyettan20296@gmail.com>
2016-10-31 11:07:52 +0700

    music

---
+++ js/loibaihat.js
@@ -0,0 +1,9 @@
+function loi(txt,title){
+  document.getElementById("loibaihat").innerHTML = txt;
+  document.getElementById("npTitle").innerHTML = title;
+  
+}
+function tieudevideo(title){
+  document.getElementById("titlevideo").innerHTML = title;
+
+}
